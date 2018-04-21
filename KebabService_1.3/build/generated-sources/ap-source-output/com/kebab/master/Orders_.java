fc7b7d30b429b811b5c426613719215c4cd75907
Guillaume Noguera <guillaume.noguera@gmail.com>
2018-04-21 18:39:31 +0200

    eeh

---
+++ KebabService_1.3/build/generated-sources/ap-source-output/com/kebab/master/Orders_.java
@@ -0,0 +1,14 @@
+package com.kebab.master;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-21T18:28:12")
+@StaticMetamodel(Orders.class)
+public class Orders_ { 
+
+    public static volatile SingularAttribute<Orders, Integer> id;
+    public static volatile SingularAttribute<Orders, Integer> state;
+
+}
